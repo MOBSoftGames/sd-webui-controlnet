6cfced5545019915b63de5f8299f55dd00b039d6
Bjørn Furuknap <furuknap@users.noreply.github.com>
2023-03-10 14:20:51 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ else
     pip install svglib
 
     git clone https://github.com/MOBSoftGames/sd-webui-controlnet $HOME/stable-diffusion-webui/extensions/sd-webui-controlnet
+    curl http://127.0.0.1:7861/sdapi/v1/restart
     echo "true";
 fi
